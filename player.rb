6310f95a8c59b2f9aa32dc5dcfdc62bac117e161
Pat George <pat.george@hungrymachine.com>
2015-09-19 12:25:50 -0400

    Add periods_played and init to 0

--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 class Player
-  attr_accessor :name, :defense
+  attr_accessor :name, :defense, :periods_played
 
   alias_method :defense?, :defense
 
   def initialize(name, defense=false)
     @name = name
     @defense = defense
+    @periods_played = 0
   end
 end
