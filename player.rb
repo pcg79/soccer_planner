32c3cc3bf2a89661f4fccfcc15460563faa3179d
Pat George <pat.george@hungrymachine.com>
2015-09-19 12:53:11 -0400

    Add method to increment played

--- player.rb
+++ player.rb
@@ -2,10 +2,15 @@ class Player
   attr_accessor :name, :defense, :periods_played
 
   alias_method :defense?, :defense
+  alias_method :defender?, :defense
 
   def initialize(name, defense=false)
     @name = name
     @defense = defense
     @periods_played = 0
+  end
+
+  def increment_played
+    @periods_played += 1
   end
 end
